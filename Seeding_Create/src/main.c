dd0dc8604518295c79d93c379c1bde7cb5b8a4ab
SusanC3 <47490118+SusanC3@users.noreply.github.com>
2020-03-06 21:07:16 -0800

    Update main.c

--- Seeding_Create/src/main.c
+++ Seeding_Create/src/main.c
@@ -14,13 +14,13 @@
 
 //effector values 
 #define arm_vert 1470
-#define arm_horz 200
+#define arm_horz 265
 #define arm_down 180
 #define arm_back 2470
 
-#define wrist_horz 453
-#define wrist_vert 1800
-#define wrist_dump 2040
+#define wrist_horz 193
+#define wrist_vert 1320
+#define wrist_dump 1756
 
 void set_up(){
     enable_servos();
@@ -35,10 +35,92 @@ void set_up(){
     //shut_down_in(119);
 }
 
+void grab_basket(){
+    
+ 	//GRAB AND DUMP BASKET
+    
+    
+   	printf("getting basket\n");
+
+    set_servo_position(arm, arm_horz);	
+    msleep(5);
+    set_servo_position(wrist, wrist_horz);
+    msleep(5);
+    printf("arm in position\n");
+
+	create_square_up_cliff(-100);
+    printf("squared up complete\n");
+    
+    msleep(100);
+    
+    create_forward(31, 100);
+ 	printf("hooked basket\n");   
+    
+    msleep(200);
+
+	servo_slow(arm, arm_horz+400);
+    msleep(5);
+    printf("basket lifted\n");
+    
+	create_backward(20, 100);
+    printf("basket dragged out");
+    
+    create_square_up_cliff(-100);
+  
+    
+    servo_slow(arm, arm_horz);
+    msleep(5);  
+    printf("basket set down\n");
+    
+    while(!side_button()) msleep(1);
+    
+	servo_slow(arm, arm_horz+400);
+    msleep(5);
+    printf("basket lifted\n");
+    
+    while(!side_button()) msleep(1);
+	
+    
+	servo_slow(wrist, wrist_vert);
+    msleep(5);
+    printf("dumped partly\n");
+    int i = 0;
+    for(i = 0; i<3; i++){
+    	servo_slow(arm, arm_horz+400);
+    	servo_slow(arm, arm_horz+300);
+    }
+    
+    while(!side_button()) msleep(1);
+    servo_slow(wrist, wrist_dump);
+    msleep(5);
+    printf("dumped full\n");
+    for(i = 0; i<3; i++){
+    	servo_slow(arm, arm_horz+400);
+    	servo_slow(arm, arm_horz+300);
+    }
+    create_disconnect();
+}
+
+void square_buttons(int speed){
+    if(left_touch == 0 && right_touch == 0){
+        create_drive_direct(speed, speed);
+    }else if(left_touch == 1 && right_touch == 0){
+        create_drive_direct( 0, speed);
+    }else if(left_touch == 0 && right_touch == 1){
+        create_drive_direct(speed, 0);
+    }
+}
+
+void forward_until_bump(int speed){
+	while(!get_create_lbump() || !get_create_rbump()) create_drive_direct(speed, speed);
+}   
+
 int main(){
     int i;
-   	//create_connect();
-    //enable_servos();
+   	create_connect();
+    enable_servos();
+    create_full();
+    create_skip_cliff();
     
   /*	set_up();
     
@@ -84,72 +166,11 @@ int main(){
     //squares up on middle black line
     //moves forward to grab basket of titantium poms
     //lifts basket up and out
-    //??? and dumps poms into funnel */
+    //??? and dumps poms into funnel 
     
-    //create gets arm */
+    //create gets arm 
     
  //   return 0;
-   
-    
-    create_connect();
-    enable_servos();
-    
-    create_full();
-    
-    create_skip_cliff();
-    
-   	printf("here\n");
-
-    set_servo_position(arm, arm_horz);	
-    msleep(5);
-    set_servo_position(wrist, wrist_horz);
-    msleep(5);
-    printf("arm in position\n");
-
-	create_square_up_cliff(-100);
-    printf("squared up complete\n");
-    
-    msleep(100);
-    
-    create_forward(31, 100);
- 	printf("hooked basket\n");   
-    
-    msleep(200);
-
-	servo_slow(arm, arm_horz+400);
-    msleep(5);
-    printf("basket lifted\n");
-    
-	create_backward(20, 100);
-    printf("basket dragged out");
-    
-    create_square_up_cliff(-100);
-    
-    while(!side_button()) msleep(1);
-	
-    
-	servo_slow(wrist, wrist_vert);
-    msleep(5);
-    printf("dumped partly\n");
-    for(i = 0; i<3; i++){
-    	servo_slow(arm, arm_horz+400);
-    	servo_slow(arm, arm_horz+300);
-    }
-    
-    while(!side_button()) msleep(1);
-    servo_slow(wrist, wrist_dump);
-    msleep(5);
-    printf("dumped full\n");
-    for(i = 0; i<3; i++){
-    	servo_slow(arm, arm_horz+400);
-    	servo_slow(arm, arm_horz+300);
-    }
-    create_disconnect();
-
-    return 0;
-    
-    create_connect();
-    enable_servos();
     
     set_servo_position(arm, arm_horz);
     msleep(5);
@@ -158,7 +179,36 @@ int main(){
     
     //create is in position to push away mine carts, facing the ravine
     create_right(45, 100);
-    create_left(45, 100);
+    create_left(45, 100); */
+    
+    //start
+    
+    servo_slow(arm, arm_horz);
+    msleep(5);
+    servo_slow(wrist, wrist_vert);
+    msleep(5);
+    
+    create_square_up_cliff(100);
+    msleep(5);
+    
+    create_forward(30, 100);
+    
+    servo_slow(arm, arm_horz+100);
+    msleep(5);
+    
+    create_forward(30, 100);
+    msleep(10);
+    
+    while(!side_button()) msleep(1);
+    
+    create_backward(20, 100);
+    
+	create_square_up_cliff(-100);
+    
+    //go to next cube
+    create_left(90, 100);
+    create_forward(30, 100);
+    create_right(90, 100);
     
     create_disconnect();
     
@@ -166,13 +216,5 @@ int main(){
     
 }
 
-void square_buttons(int speed){
-    if(left_touch == 0 && right_touch == 0){
-        create_drive_direct(speed, speed);
-    }else if(left_touch == 1 && right_touch == 0){
-        create_drive_direct( 0, speed);
-    }else if(left_touch == 0 && right_touch == 1){
-        create_drive_direct(speed, 0);
-    }
-}
+
 
